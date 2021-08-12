4cf75770648651cf30b3fc6b75c24616b4853d5b
Stephanie Flora <twinsf2@gmail.com>
2021-08-11 21:59:00 -0700

    added the second function

--- src/script.js
+++ src/script.js
@@ -5,11 +5,12 @@
 
 const getMoviesBySearchTerm = async (x) => {
     try {
-        const input = document.querySelector('input');
+        const input = document.getElementById('inputSrch');
         // console.log(input.value)
         // searchTerm = e.target.value
         searchTerm = input.value;
         //console.log(searchTerm);
+        
         let response = await fetch(`http://www.omdbapi.com/?apikey=${apikey}&s=${searchTerm}`);
         let omdires = await response.json();
         //console.log(omdires.Search.length);
@@ -22,7 +23,7 @@ const getMoviesBySearchTerm = async (x) => {
         for (let i = 0; i < search.length; i++) {
             //console.log(search[i])
             let div = document.createElement("DIV");
-            div.innerHTML = `<h3>Search Results</h3><hr><h4> ${search[i].Title} </h4><p>Year: ${search[i].Year} </p><img src="${search[i].Poster}" alt="no poster"><hr>`
+            div.innerHTML = `<h3>Search Results</h3><hr><h4> ${search[i].Title} </h4><p>Year: ${search[i].Year} </p><p>imdb ID: ${search[i].imdbID} </p><img src="${search[i].Poster}" alt="no poster"><hr>`
             searchDiv.appendChild(div);
           } 
         // console.log('This is the last line of code in this function');
@@ -31,7 +32,50 @@ const getMoviesBySearchTerm = async (x) => {
       }
 }
 
+const getMovieDetailsById = async (x) => {
+    try {
+        const input = document.getElementById('inputId');
+        //console.log(input.value)
+        // searchTerm = e.target.value
+        searchTerm = input.value;
+        //console.log(searchTerm);
+        
+        let response = await fetch(`http://www.omdbapi.com/?apikey=${apikey}&i=${searchTerm}`);
+        let omdires = await response.json();
+        //console.log(omdires);
+        
+        const searchDiv = document.getElementById('searchResults');
+        searchDiv.innerHTML =  '';
+
+        const search = omdires;
+
+        //console.log(search)
+        let div = document.createElement("DIV");
+        div.innerHTML = `
+        <h3>Search Results</h3>
+        <hr>
+        <h4> ${search.Title} </h4>
+        <p><strong>Year:</strong> ${search.Year} </p>
+        <p><strong>imdb ID:</strong> ${search.imdbID} </p>
+        <p><strong>Rated:</strong> ${search.Rated} <strong>Metascore:</strong> ${search.Metascore}  <strong>imdb Rating:</strong> ${search.imdbRating} </p>
+        <p><strong>Actors:</strong> ${search.Actors} </p>
+        <p><strong>Plot:</strong> ${search.Plot} </p>
+        <p><strong>Runtime:</strong> ${search.Runtime} </p>
+        <img src="${search.Poster}" alt="no poster">
+        <hr>
+        
+        
+        `
+        searchDiv.appendChild(div);
+
+            // console.log('This is the last line of code in this function');
+      } catch(error) {
+        console.log("ERROR:", error)
+      }
+}
+
 document.getElementById("btn").addEventListener("click", getMoviesBySearchTerm);
+document.getElementById("btnid").addEventListener("click", getMovieDetailsById);
 
 
 // input.addEventListener('input', updateValue);
